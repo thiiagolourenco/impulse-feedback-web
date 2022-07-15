d6fb2ba0af1080920e3aba394b22f09dc08a4294
Thiago Lourenço <51346622+thiiagolourenco@users.noreply.github.com>
2022-07-15 12:07:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    minify: "terser",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 550,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "react-dom",
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+});
